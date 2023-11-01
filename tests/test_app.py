29b36abe366d6d656d338840bb99ca85167985a8
Rubens-dee <ruben.docter@gmail.com>
2023-11-01 17:28:08 +0100

    upload

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from conftest import client
+
+
+def test_should_status_code_ok(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    response = client.get('/form')
+    assert response.status_code == 200
+    response = client.post('/submit', data={"name": "Ruben"})
+    assert response.status_code == 200
