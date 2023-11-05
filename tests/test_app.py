4d1b0131f4da7a4b5155db238c54ae83ecff1a25
Rubens-dee <ruben.docter@gmail.com>
2023-11-05 12:04:12 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,5 +6,5 @@ def test_should_status_code_ok(client):
     assert response.status_code == 200
     response = client.get('/form')
     assert response.status_code == 200
-    response = client.post('/submit', data={"name": "Ruben"})
+    response = client.post('/submit', data={"name": "Ruben1"})
     assert response.status_code == 200
